49a395459205526e75887226f77e87d8a3597097
Cuda-Chen <clh960524@gmail.com>
2020-04-06 09:51:21 +0800

    add submodule fftw_module
    1. Add submodule fftw_module.
    2. Remove `fft.c` and `fft.h` because I put them to fftw_module.

--- fft.h
+++
@@ -1,9 +0,0 @@
-#ifndef FFT_H
-#define FFT_H
-
-void fftToFileHalf (double *data, uint64_t dataSamples, double sampleRate, FILE *fptr);
-void fftToFile (double *data, uint64_t dataSamples, double sampleRate, FILE *fptr);
-void testFFT (double *data, fftw_complex *in, fftw_complex *out,
-              fftw_complex *ref, uint64_t totalSamples);
-
-#endif
